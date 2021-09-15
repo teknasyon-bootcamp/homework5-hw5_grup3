3b789fbbf11ddbc6f391b812450b59dd9719e604
ancyra2 <duran.talih@gmail.com>
2021-09-15 16:30:30 +0300

    Database sistemi ayaklandırıldı

--- app/DB/Database.php
+++ app/DB/Database.php
@@ -13,19 +13,19 @@ class Database
         $this->driver = $driver;
     }
     public function all(string $table): array{
-
+        return $this->driver->all($table);
     }
     public function find(string $table, mixed $id):mixed{
-
+        return $this->driver->find($table, $id);
     }
     public function create(string $table, array $values): bool{
-
+        return $this->driver->create($table, $values);
     }
     public function update(string $table, mixed $id, array $values): bool{
-
+        return $this->driver->update($table, $id, $values);
     }
     public function delete(string $table, mixed $id): bool{
-
+        return $this->driver->delete($table, $id);
     }
 
 
